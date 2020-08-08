cddc90c17fef796ed1424f67567affcfd3b908d3
ram <ram.ram@rama.com>
2020-08-08 06:15:54 +0000

    commiting changes from One and abc.txt

--- One.java
+++ One.java
@@ -4,5 +4,6 @@ public static void main(String args[])
 {
 System.out.println("In Java class One");
 System.out.println("Adding a new line in One.java file")
+/* Adding a line for the Stash purpose*/
 }
 }
