09c7fcaf11f44fec7f4ee7b1a80ace0f7684da84
ram <ram.ram@rama.com>
2020-08-08 06:47:28 +0000

    commiting into repo

--- One.java
+++ One.java
@@ -5,5 +5,7 @@ public static void main(String args[])
 System.out.println("In Java class One");
 System.out.println("Adding a new line in One.java file")
 /* Adding a line for the Stash purpose*/
+
+/*Undoing changes from Remote Repo2*/
 }
 }
